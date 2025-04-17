8474fc255572317d51f202e88793194e01f8cef9
doston <dostonadxamov@222gmail.com>
2025-04-17 16:10:33 +0500

    initial commit

---
+++ js/appps.js
@@ -0,0 +1,301 @@
+// ================================
+// 1-masala
+// ================================
+
+// const user = {
+//     name: "ali",
+//     age: 25,
+// }
+
+// function f(user) {
+//     return `ismi ${user.name} yoshi ${user.age}`;
+// }
+
+// console.log(f(user));
+
+
+
+// ================================
+// 2-masala
+// ================================
+// const a ={
+//  name:"telefon",
+//  price: 1000,
+// }
+
+// function f() {
+//     a.pricewithTax = a.price % 12
+//     return`name ${a.name} price ${a.price} pricewithTax ${a.pricewithTax}`
+// }
+// console.log(f());  shu ikkini qilolmadm ustoz
+
+
+
+// ================================
+// 3-masala
+// ================================
+
+// const car = {
+//     color:"oq",
+//     year: 2022,
+//     name: "cobalt",
+// }
+
+// function f(cars) {
+//     return `rusum ${car.name} year ${car.year} color ${car.color}`
+// }
+
+// console.log(f(car));
+
+// ================================
+// 4-masala
+// ================================
+
+// const a = {
+//     name: "ali",
+// }
+
+// function f(email) {
+//     return a.name in email
+// }
+
+// console.log(f(a));
+
+// ================================
+// 5-masala
+// ================================
+
+// const a ={
+//     name1: "ali", salary: 1000, 
+//     name2: "vali", salary2: 1500
+// }
+
+
+// function s(salary) {
+//     return a.salary+a.salary2
+// }
+
+// console.log(s());
+
+// ================================
+// 6-masala
+// ================================
+
+// const a = {
+//     model:"cobalt",
+//     distance:150,
+//     time:3,
+// }
+
+
+// function f(car) {
+// a.speed = a.distance/a.time
+// return `distance ${a.distance}
+// time ${a.time}
+// speed ${a.speed}`
+// }
+
+// console.log(f(a))
+
+
+
+// ================================
+// 7masala
+// ================================
+
+// const obj = {
+//     name:"ali",
+//     start: 9,
+//     end:18,
+// }
+
+// obj.hours = obj.end-obj.start
+
+// function f(calc) {
+//     return `name ${obj.name} start ${obj.start}  end ${obj.end} hours ${obj.hours}`
+// }
+
+// console.log(f());
+
+
+
+// ================================
+// 8masala
+// ================================
+
+// const v ={
+//     side:prompt("son kiritng")
+// }
+
+// function kub() {
+//     v.volume = v.side*v.side*v.side
+//     return `sude ${v.side} volume ${v.volume}`
+// }
+
+// console.log(kub());
+
+
+// ================================
+// 10 masala
+// ================================
+
+// const a ={
+//     name:"laylo",
+//     age:20,
+// }
+
+
+// function f() {
+//     a.birthday = 2025-a.age
+//     return `name ${a.name}  age ${a.age}   year ${a.birthday}`
+// }
+// console.log(f());
+
+
+// ================================
+// 9 masala
+// ================================
+// const a ={
+//     "user name": "tets",
+//     pasword:"12345678",
+//     istrong:"" 
+// }
+
+// function n() {
+//     if (a.pasword.length==8) {
+//         a.istrong = true
+//     }
+//     else{
+//         a.istrong=false
+//     }
+// }
+
+// n()
+// console.log(a);
+
+
+// ================================
+// 11 masala
+// ================================
+
+// const a = {
+//     name:"ulugbek",
+//     age:"20"
+// }
+
+// Object.freeze(a)
+
+// function f() {
+//     if ( Object.isFrozen(a)==true){
+//         console.log("object is frozen");
+        
+//     }
+//     else{
+//         console.log("object isnot frozen");
+//     }
+// }
+
+// f()
+
+// ================================
+// 12 masala
+// ================================
+
+// const a = {
+//     model:"malibu"
+//     }
+    
+//     Object.freeze(a)
+    
+//     function f() {
+//         if ( Object.isFrozen(a)==true){
+//             console.log("frozen");
+            
+//         }
+//         else{
+//             console.log("not frozen");
+//         }
+//     }
+    
+//     f()
+
+// ================================
+// 13 masala
+// ================================
+
+// const a = {
+//     name:"ali"
+// }
+
+// Object.seal(a)
+// a.name =20
+// function f() {
+//     if ( Object.isSealed(a)==true){
+//         console.log("cannot add new prporty");
+
+//     }
+//     else{
+//         console.log("saved new proporty");
+//     }
+
+// }
+
+// shuni qilolmadm
+
+// f()
+
+// ================================
+// 14 masala
+// ================================
+
+// const a = {
+//     brand:"iphone"
+//     }
+    
+//     Object.seal(a)
+    
+//     function f() {
+//         if ( Object.isSealed(a)==true){
+//             console.log("sealed");
+            
+//         }
+//         else{
+//             console.log("not sealed");
+//         }
+//     }
+    
+// f()
+
+
+// ================================
+// 15 masala
+// ================================
+
+// const a ={
+// title:"js fundamental",
+// author:"mirzo",
+// }
+
+// function f() {
+//  return "title"  in a
+// }
+
+// console.log(f());
+
+
+
+// ================================
+// 16 masala
+// ================================
+
+// const a ={
+//     name:"ali",
+//     age:20,
+
+// }
+
+// function f() {
+    
+// }
+
+// ustoz shuni qilomadm 
